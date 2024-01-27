deb9cbc3e63f7fc11435a622eb7a8c98b2546058
Gabriel Silva <gabrielmdhtt@gmail.com>
2024-01-27 16:50:17 -0300

    feat: mvp

---
+++ src/app/actions.ts
@@ -0,0 +1,65 @@
+"use server";
+
+import { google } from "googleapis";
+
+import { FormValues } from "./page";
+
+export async function updateSheet(formData: FormValues) {
+	try {
+		const auth = new google.auth.GoogleAuth({
+			credentials: {
+				type: process.env.TYPE,
+				project_id: process.env.PROJECT_ID,
+				private_key_id: process.env.PRIVATE_KEY_ID,
+				private_key: (process.env.PRIVATE_KEY as string).replace(/\\n/g, "\n"),
+				client_email: process.env.CLIENT_EMAIL,
+				client_id: process.env.CLIENT_ID,
+				universe_domain: process.env.UNIVERSE_DOMAIN,
+			},
+			scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+		});
+
+		const sheetsClient = google.sheets({ version: "v4", auth });
+
+		const spreadsheetId = process.env.SPREADSHEET_ID;
+		const sheetName = "Sheet1";
+
+		const values = [
+			[
+				formData.name,
+				formData.dayEvaluation,
+				formData.materialsProduced,
+				formData.planForTomorrow,
+				formData.observations,
+			],
+		];
+
+		const gaxiosResponse = await sheetsClient.spreadsheets.values.append({
+			spreadsheetId,
+			range: `${sheetName}!A2:E2`,
+			valueInputOption: "USER_ENTERED",
+			includeValuesInResponse: true,
+			requestBody: {
+				values,
+			},
+		});
+
+		if (gaxiosResponse.status !== 200) {
+			return {
+				status: "error",
+				message: "Error updating sheet",
+				statusCode: gaxiosResponse.status,
+				data: gaxiosResponse.data,
+			} as const;
+		}
+
+		return {
+			status: "success",
+			message: "Successfully updated sheet",
+			statusCode: gaxiosResponse.status,
+			data: gaxiosResponse.data,
+		} as const;
+	} catch (error) {
+		throw new Error(`Error in updateSheet method: ${error}`);
+	}
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,163 @@
-import Image from 'next/image'
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "../components/ui/textarea"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select"
+import { updateSheet } from "./actions"
+import { useToast } from "../components/ui/use-toast"
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Obrigatorio",
+  }),
+  dayEvaluation: z.enum(['Produtivo', 'Neutro', 'Improdutivo'], {
+    required_error: "Obrigatorio",
+    description: "Como você considera seu dia hoje?",
+  }),
+  materialsProduced: z.string({
+    required_error: "Obrigatorio",
+    description: "Quantos materiais você conseguiu produzir hoje?"
+  }).min(1, { message: "Obrigatorio" }),
+  planForTomorrow: z.string({
+    required_error: "Obrigatorio",
+    description: "No que pretende trabalhar amanhã?"
+  }).min(2, {
+    message: "Obrigatorio",
+  }),
+  observations: z.string({
+    description: "Observações"
+  }).optional()
+});
+
+export type FormValues = z.infer<typeof formSchema>;
 
 export default function Home() {
+  const { toast } = useToast();
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    mode: "onBlur",
+  });
+
+  const onSubmit = async (values: FormValues) => {
+    const updateSpreadsheet = await updateSheet(values);
+
+    if(updateSpreadsheet.status === "error") {
+      toast({
+        title: "Erro ao enviar formulário",
+        description: "Tente novamente mais tarde",
+        duration: 5000,
+        variant: "destructive",
+      });
+      return;
+    }
+
+    if(updateSpreadsheet.status === "success") {
+      toast({
+        title: "Formulário enviado com sucesso",
+        duration: 5000,
+      });
+
+      form.setValue("name", "");
+      form.setValue("dayEvaluation", "Neutro");
+      form.setValue("materialsProduced", "");
+      form.setValue("planForTomorrow", "");
+      form.setValue("observations", "");
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex justify-center items-center min-h-screen bg-slate-100">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full max-w-md p-8 bg-white shadow-md rounded-md">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input placeholder="Bruce Wayne" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <FormField
+            control={form.control}
+            name="dayEvaluation"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Como foi seu dia hoje?</FormLabel>
+                <FormControl>
+                  <Select {...field} onValueChange={field.onChange} defaultValue={field.value}>
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Selecione" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="Produtivo">Produtivo</SelectItem>
+                      <SelectItem value="Neutro">Neutro</SelectItem>
+                      <SelectItem value="Improdutivo">Improdutivo</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+          <FormField
+            control={form.control}
+            name="materialsProduced"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Quantos materiais você produziu hoje?</FormLabel>
+                <FormControl>
+                  <Input placeholder="2" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+          <FormField
+            control={form.control}
+            name="planForTomorrow"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>No que pretende trabalhar amanhã?</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Pretendo..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+          <FormField
+            control={form.control}
+            name="observations"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="flex flex-row gap-2">Observações <p className="text-gray-500">(opcional)</p></FormLabel>
+                <FormControl>
+                  <Textarea placeholder="..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+          <Button type="submit" aria-disabled={form.formState.isLoading}>Enviar</Button>
+        </form>
+      </Form>
     </main>
   )
 }
